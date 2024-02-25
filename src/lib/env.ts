1129444263e1432bba9c487bda98111ab48bb95e
RaNiiJohnson <johnsontolotriniavo@gmail.com>
2024-02-25 14:17:18 +0300

    fix env

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -5,14 +5,10 @@ export const env = createEnv({
   server: {
     GITHUB_ID: z.string().min(1),
     GITHUB_SECRET: z.string().min(1),
-    FACEBOOK_ID: z.string().min(1),
-    FACEBOOK_SECRET: z.string().min(1),
   },
   client: {},
   runtimeEnv: {
     GITHUB_ID: process.env.GITHUB_ID,
     GITHUB_SECRET: process.env.GITHUB_SECRET,
-    FACEBOOK_ID: process.env.FACEBOOK_ID,
-    FACEBOOK_SECRET: process.env.FACEBOOK_SECRET,
   },
 });
